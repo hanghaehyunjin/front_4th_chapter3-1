785ac1edfbfb08ece1c6d90689006aff9157e720
hyunjin <guswns1419@naver.com>
2025-02-05 20:24:47 +0900

    feat : fetchHolidays 테스트 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,24 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date(2024, 4, 1);
+    const result = fetchHolidays(date);
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+    expect(result).toEqual({ '2024-05-05': '어린이날' });
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date(2024, 6, 1);
+    const result = fetchHolidays(date);
+
+    expect(result).toEqual({});
+  });
+
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date(2024, 8, 1);
+    const result = fetchHolidays(date);
+
+    expect(result).toEqual({ '2024-09-16': '추석', '2024-09-17': '추석', '2024-09-18': '추석' });
+  });
 });
